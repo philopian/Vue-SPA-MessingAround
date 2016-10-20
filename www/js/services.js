f33b16abeb45c9f3bcaae6423084f120c495019f
philopian <pdotwdot@gmail.com>
2016-10-20 16:16:11 -0700

    re-factor the creation of the components

--- www/js/services.js
+++ www/js/services.js
@@ -1,87 +1,56 @@
-// return new Promise(function(resolve, reject) {
-//     $.get(BASE_URL + "D.ashx?api=projects", function(data, status) {
-//       if (status == "success") {
-//         resolve(data);
-//       } else {
-//         reject();
-//       }
+// function getHelloHtml() {
+//   return new Promise(function(resolve, reject) {
+//     $.get('hello.html', function(html) {
+//       Vue.component('hello-component', {
+//         template: html,
+//         created: function() {
+//         }
+//       });
+//       resolve();
 //     });
 //   });
+// }
 //
+// function getPage2Html() {
+//   return new Promise(function(resolve, reject) {
+//     $.get('page2.html', function(html) {
+//       Vue.component('page2-component', {
+//         template: html,
+//         created: function() {
+//         }
+//       });
+//       resolve();
+//     });
+//   });
+// }
 
 
-function getHelloHtml() {
+function initComponent(html,compmentName){
   return new Promise(function(resolve, reject) {
-    $.get('hello.html', function(data) {
-      // Vue.component('hello-component', {
-      //   template: data,
-      //   created: function() {
-      //     console.log("...........CREATED....hello.html");
-      //     resolve();
-      //   }
-      // });
-      resolve(data);
+    $.get(html, function(htmlData) {
+      Vue.component(compmentName, {
+        template: htmlData,
+        created: function() {
+        }
+      });
+      resolve();
     });
   });
 }
 
-function getPage2Html() {
-  return new Promise(function(resolve, reject) {
-    $.get('page2.html', function(data) {
-      // Vue.component('page2-component', {
-      //   template: data,
-      //   created: function() {
-      //     console.log("...........CREATED....page2.html");
-      resolve(data);
-      //   }
-      // });
-    });
-  });
-}
+
+
+
 
 var app = {};
-getHelloHtml()
-  .then(function(html) {
-    Vue.component('hello-component', {
-      template: html,
-      created: function() {
-        console.log("...........CREATED....html.html");
-      }
-    });
-    console.log("...then1");
-    return getPage2Html();
+initComponent('hello.html' , 'hello-component')
+  .then(function() {
+    return initComponent('page2.html' , 'page2-component')
   })
-  .then(function(html) {
-    console.log("...then2");
-    Vue.component('page2-component', {
-      template: html,
-      created: function() {
-        console.log("...........CREATED....html.html");
-      }
-    });
-  })
+  .then(function() {})
   .then(function() {
     console.log("all the files were created");
     app = new Vue({
       el: '#main'
     });
-  })
-
-//
-// $.get('hello.html', function(data) {
-//   Vue.component('hello-component', {
-//     template: data,
-//     created: function() {
-//       // add some JS code here
-//       console.log("......CREATED....hello.html")
-//     }
-//   });
-// });
-
-
-// Promise.all([getHelloHtml(),getPage2Html()]).then(function() {
-//   console.log("all the files were created");
-//   // var app = new Vue({
-//   //   el: '#main'
-//   // });
-// });
+  });
