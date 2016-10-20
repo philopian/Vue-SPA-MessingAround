48fd8e2dd9b5bf42a75accd3deb5428fc8c907f8
philopian <pdotwdot@gmail.com>
2016-10-20 16:58:15 -0700

    added vue-router

--- www/js/hello.js
+++ www/js/hello.js
@@ -1,79 +1,50 @@
 console.log("....   hello.js")
-
-// $.get('hello.html', function(data) {
-//   console.log(data)
-//
-//   // create compnents
-//   Vue.component('hello-component', {
-//     // template: data,
-//     template: '<p>sdfsdfsd</p>',
-//     // methods: {},
-    // render: function() {
-    //         console.log("......hello.js RENDER method")
-    // }
-//   })
-//
-//   // create a root instance
-//   var app = new Vue({
-//     el: '#main'
-//   });
-// });
+/**************************************************************************************/
+/*    PAGE SPECIFIC GLOBAL VARIABLES                                                  */
+/**************************************************************************************/
+var map = {};
 
 
-// $(document).on('click', '#btn-append-to-component', appendToComponent)
-//
-// function appendToComponent(){
-//   console.log("......")
-//   $("#hello-view p ").append('<p> (... with to compoment with timeout)</p>')
-// }
+/**************************************************************************************/
+/*    DOCUMENT READY                                                                  */
+/**************************************************************************************/
+$(document).ready(function() {
 
-$(document).on('click', '#btn-append-to-component', appendToComponent)
+});
 
-function appendToComponent(){
-  console.log("......")
-  $("#hello-view p ").empty().append('<p> (... with to compoment with timeout)</p>')
+
+/**************************************************************************************/
+/*    WINDOW RESIZE                                                                   */
+/**************************************************************************************/
+var doit;
+window.onresize = function() {
+  clearTimeout(doit);
+  doit = setTimeout(calculateMapDevHeight, 100);
+};
+
+
+
+/**************************************************************************************/
+/*    FUNCTIONS                                                                       */
+/**************************************************************************************/
+function calculateMapDevHeight() {
+
 }
 
+/**************************************************************************************/
+/*    CLICK                                                                           */
+/**************************************************************************************/
+
+
+
+
+/**************************************************************************************/
+/*    DEV DEV DEV                                                                     */
+/**************************************************************************************/
+
+
+
+
+
+
 //
-// $.get('hello.html', function(data) {
-//     Vue.component('hello-component', {
-//       template: data,
-//       created: function(){
-//           // add some JS code here
-//           console.log("......CREATED")
-//
-//
-//
-//
-//
-//
-//           // $("#hello-view").css({
-//           //   "color": "red"
-//           // });
-//           //
-//           // $("#hello-view p ").append('(Appened with to compoment with timeout)')
-//           //
-//           // setTimeout(function(){
-//           //     $("#hello-view p ").append('<p> (Appened with to compoment with timeout)</p>')
-//           // }, 2000)
-//           //
-//           //
-//           // $("body").append('<br /><p> APPEND!!!!!!!!!!!!!</p>')
-//           //
-//           //
-//           //  $("p").append("<br /><b>Appended text</b>");
-//           //
-//           // // $(document).append('<p> appened</p>')
-//           //
-//           // $(document).on('click', "#hello-view", function(){
-//           //   console.log(">>>>>CLICK  ")
-//           // })
-//
-//
-//
-//
-//
-//       }
-//   });
-//
-// });
--- www/js/services.js
+++ www/js/services.js
@@ -1,56 +1,118 @@
-// function getHelloHtml() {
-//   return new Promise(function(resolve, reject) {
-//     $.get('hello.html', function(html) {
-//       Vue.component('hello-component', {
-//         template: html,
-//         created: function() {
-//         }
-//       });
-//       resolve();
-//     });
-//   });
-// }
-//
-// function getPage2Html() {
-//   return new Promise(function(resolve, reject) {
-//     $.get('page2.html', function(html) {
-//       Vue.component('page2-component', {
-//         template: html,
-//         created: function() {
-//         }
-//       });
-//       resolve();
-//     });
-//   });
-// }
-
-
-function initComponent(html,compmentName){
+function initComponent(html, compmentName) {
   return new Promise(function(resolve, reject) {
     $.get(html, function(htmlData) {
-      Vue.component(compmentName, {
+      var component = Vue.component(compmentName, {
         template: htmlData,
-        created: function() {
-        }
+        created: function() {}
       });
-      resolve();
+      resolve(component);
     });
   });
 }
 
-
-
-
-
 var app = {};
-initComponent('hello.html' , 'hello-component')
-  .then(function() {
-    return initComponent('page2.html' , 'page2-component')
+var components = {};
+initComponent('hello.html', 'hello')
+  .then(function(component) {
+    components.hello = component;
+
+    return initComponent('page2.html', 'page2')
   })
-  .then(function() {})
+  .then(function(component) {
+      components.page2 = component;
+  })
   .then(function() {
     console.log("all the files were created");
-    app = new Vue({
-      el: '#main'
+    // Register all components
+    // app = new Vue({
+    //   el: '#main'
+    // });
+
+    // router
+    var App = Vue.extend({})
+    var router = new VueRouter({
+      root: '/',
+      hashbang: false,
+      mode: 'html5',
+      history: false,
+      linkActiveClass: "active"
     });
+
+
+    router.map({
+      '/foo': {
+        component: components.hello
+      },
+      '/bar': {
+        component: components.page2
+      }
+    })
+
+
+
+    router.start(App, '#app')
   });
+
+
+//
+// // Define some components
+// var Foo = Vue.extend({
+//   template: '<p>This is foo!</p>'
+// })
+//
+// var Bar = Vue.extend({
+//   template: '<p>This is bar!</p>'
+// })
+//
+// var App = Vue.extend({})
+// var router = new VueRouter({
+//   root: '/',
+//   hashbang: false,
+//   mode: 'html5',
+//   history: false,
+//   linkActiveClass: "active"
+// });
+//
+//
+// router.map({
+//   '/foo': {
+//     component: Foo
+//   },
+//   '/bar': {
+//     component: Bar
+//   }
+// })
+//
+//
+//
+// router.start(App, '#app')
+//
+
+
+
+
+
+
+
+
+//
+// var App = Vue.extend({});
+// var router = new VueRouter();
+//
+// var Home = Vue.extend({
+//   template: 'Welcome to the <b>home page</b>!'
+// });
+// var People = Vue.extend({
+//   template: 'Look at all the people who work here!'
+// });
+//
+// router.map({
+//   '/': {
+//     component: Home
+//   },
+//   '/people': {
+//     component: People
+//   }
+// });
+//
+// router.start(App, '#app');
